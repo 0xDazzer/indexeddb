3bf6fb099968e6a42fdb5430ba74d6460d2bbc05
Vakulio <ss>
2026-01-13 20:47:42 +0300

    on and once implementation

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
-async function on<T>(eventTarget: EventTarget, eventName: string): Promise<T> {
+function once<T>(eventTarget: EventTarget, eventName: string): Promise<T> {
+  return new Promise<T>((resolve) => {
+    eventTarget.addEventListener(eventName, (event) => resolve(event as T), { once: true });
+  });
+}
+
+function on<T>(eventTarget: EventTarget, eventName: string): AsyncIterableIterator<T> {
+  return {
+    async next(): Promise<IteratorResult<T>> {
+      return { value: await once<T>(eventTarget, eventName), done: false };
+    },
+    [Symbol.asyncIterator](): AsyncIterableIterator<T> {
+      return this;
+    }
+  };
 }
