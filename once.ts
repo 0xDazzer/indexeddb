6905d783bf1f9e836875fcf0dc6937c85e59eb95
Bakhodir Kadyrov <bahobek@gmail.com>
2026-01-14 12:46:16 +0500

    remove handler from once method

--- once.ts
+++ once.ts
@@ -1,11 +1,8 @@
 function once<T>(eventTarget: EventTarget, eventName: string): Promise<T>{
-    const handler  = (resolve: (value: T | PromiseLike<T>) => void) => (event: T) => {
-        console.log(`Event "${eventName}" occurred.`);
-        resolve(event);
-    };
-
     return new Promise<T>((resolve) => {
-        eventTarget.addEventListener(eventName, (event) => handler(resolve)(event as T), { once: true });
+        eventTarget.addEventListener(eventName, (event) => {
+            resolve(event as unknown as T);
+        }, { once: true });
     }
     );
 }
